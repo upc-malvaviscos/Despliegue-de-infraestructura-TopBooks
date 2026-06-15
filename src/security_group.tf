5d3c09bc595711d4db794fa4d025010cd437ec5b
renato <or2ll@duck.com>
2026-06-15 20:45:33 +0200

    feat: define security group for web server with HTTP and HTTPS access

--- src/security_group.tf
+++ src/security_group.tf
@@ -1,5 +1,32 @@
 # Grupo de Seguridad para el Servidor Web 
+resource "aws_security_group" "web_sg" {
+  name        = "topbooks-web-sg"
+  description = "Permitir HTTP y HTTPS desde el exterior"
+  vpc_id      = aws_vpc.main_vpc_topbooks_malvaviscos.id
 
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = merge(local.tags_obligatorios, { Name = "topbooks-web-sg" })
+}
 
 # GRUPO DE SEGURIDAD DE LA BD 
 resource "aws_security_group" "db_sg" {
